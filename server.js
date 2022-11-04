3e99b01e6737f7206fa9b2246599fcfd422bab0a
payyavulapradeep <100224234+payyavulapradeep@users.noreply.github.com>
2022-11-04 12:23:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello docker container');
+  res.send('Hello bangalore');
 });
 
 app.listen(PORT, HOST);
